866b8082569ba92f399f7a380f4681e760641e6f
0123Pavankumar <128778236+0123Pavankumar@users.noreply.github.com>
2024-12-19 09:43:47 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+import "./styles.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Portfolio Tracker</h1>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
